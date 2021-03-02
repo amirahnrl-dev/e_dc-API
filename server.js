1ebd53eb22e6ef70b496a6adcab7f633e6d17d13
Amirah Nrl <seasheep@outlook.com>
2021-03-02 09:30:11 +0800

    morgan, 3rd party logger

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const morgan = require('morgan');
 const bootcamps = require('./routes/bootcamps');
 
 dotenv.config({ path: './config/config.env' });
+
+if (process.env.NODE_ENV === 'development') {   // DEV-ONLY
+    app.use(morgan('dev'));
+}
 
 app.use('/api/v1/bootcamps', bootcamps);
 
