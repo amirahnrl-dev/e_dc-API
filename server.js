a22f86e8d9f250c78192e363db570f6bd29b19df
Amirah Nrl <seasheep@outlook.com>
2021-03-02 12:15:32 +0800

    connect to db with mongoose

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const app = express();
-const PORT = process.env.PORT || 5000;
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 const bootcamps = require('./routes/bootcamps');
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
 
 if (process.env.NODE_ENV === 'development') {   // DEV-ONLY
     app.use(morgan('dev'));
@@ -13,9 +17,18 @@ if (process.env.NODE_ENV === 'development') {   // DEV-ONLY
 
 app.use('/api/v1/bootcamps', bootcamps);
 
-app.listen(
+const server = app.listen(
     PORT, 
     console.log(
         `Server running in ${ process.env.NODE_ENV } mode on port ${ PORT }`
     )
+);
+
+/* HANDLE UNHANDLED PROMISE REJECTION */
+process.on('unhandledRejection', (err, promise) => {
+        console.log(`Error: ${ err.message }`);
+
+        // close server & exit process
+        server.close(() => process.exit(1));
+    }
 );
