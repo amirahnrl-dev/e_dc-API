6e424f639de11a8607dc79ae764ce6432a158fc6
Amirah Nrl <seasheep@outlook.com>
2021-03-05 10:33:16 +0800

    Populate courses with bootcamps data, virtual populate bootcamps with courses , cascade delete courses for deleted bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -18,7 +18,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
             match => `$${ match }`
         );
 
-        let query = Bootcamp.find(JSON.parse(queryStr)); // string to JSON
+        let query = Bootcamp.find(JSON.parse(queryStr)).populate('courses'); // string to JSON
 
         if(req.query.select) {
             const fields = req.query.select.split(',').join(' ');
@@ -173,7 +173,7 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @method      DELETE /api/v1/bootcamps/:id
 // @access      Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+        const bootcamp = await Bootcamp.findById(req.params.id);
     
         if(!bootcamp) {
             return next(
@@ -183,6 +183,9 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
                 )
             );
         }
+
+        // triggers pre() middleware
+        bootcamp.remove();
     
         res.status(200).json(
             {
