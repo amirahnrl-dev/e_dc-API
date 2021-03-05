9c8214f5a4203c1a7af96aa2aecb0e085ab82d22
Amirah Nrl <seasheep@outlook.com>
2021-03-05 12:47:38 +0800

    CRUD for courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });   // rerouting from bootcamps route
 const {
-    getCourses
+    getCourses,
+    getCourse,
+    createCourse,
+    updateCourse,
+    deleteCourse
 } = require('../controllers/courses');
 
-router.route('/').get(getCourses);
+router
+.route('/')
+.get(getCourses)
+.post(createCourse);
+
+router
+.route('/:id')
+.get(getCourse)
+.put(updateCourse)
+.delete(deleteCourse);
 
 module.exports = router;
